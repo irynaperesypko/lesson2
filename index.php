cebda4ec5a63920acc13c2ceb81a6f91b4402f5a
irynaperesypko <peresypko.irina@gmail.com>
2021-07-18 23:18:37 +0300

    fixed operation division  and optimisation compare strings

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Получить остаток деления 7 на 3=" . (7 / 3) . "<br/>";
+echo "Получить остаток деления 7 на 3=" . (7 % 3) . "<br/>";
 echo "Получить целую часть сложения 7 и 7,15=" . (int)(7 + 7.15) . "<br/>";
 echo "Получить корень из 25=" . sqrt(25) . "<br/>";
 echo "<hr><br/><br/>";
@@ -21,7 +21,15 @@ echo true == 1 ? "yes" : "no";
 echo "<br/>Правильно ли утверждение  false тождественно 0 ";
 echo false === 0 ? "yes" : "no" . "<br/>";
 echo "Какая строка длиннее three - три ";
-echo strlen('three') == mb_strlen('три') ? 'three==три' : (strlen('three') > mb_strlen('три') ? 'three>три' : 'three<три');
+$strLen1 = strlen('three');
+$strLen2 = mb_strlen('три');
+if ($strLen1 == $strLen2) {
+    echo 'three==три';
+} elseif ($strLen1 > $strLen2) {
+    echo 'three>три';
+} else {
+    echo 'three<три';
+}
 echo "<br/>";
 echo "Какое число больше 125 умножить на 13 плюс 7 или 223 плюс 28 умножить на 2 ";
 if ((125 * 13 + 7) == (223 + 28 * 2)) {
